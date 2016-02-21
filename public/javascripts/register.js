73ef19575b1d7b61d4a92bf24ddec55bc9013ae7
jichangfeng67 <jichangfeng67@gmail.com>
2016-02-21 16:53:16 +0100

    update the information check method in register.jade javascript

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,14 +1,35 @@
 $(document).ready(function(){
-  /**
-    *this is to get the password and compares if they are the same
-  */
-  $("input[name='confirmpwd']").keyup(function(){
-    var pwd = $("input[name='pwd']").val();
-    var cpwd = $(this).val();
-    if (pwd === cpwd) {
+
+  $("input").keyup(function(){
+    if (pwdConfirm() && 1) {
       $("#confirmpwdwrong").html("");
+      $("input[type='submit']").prop("disabled", false);
     }else {
-      $("#confirmpwdwrong").html("Password not accept");
+      $("#confirmpwdwrong").html("Password not compatiable");
     }
   })
 })
+
+var regPost = function(){
+  var id = $("input[name='id']").val();
+  var pwd = $("input[name='pwd']").val();
+  $.ajax({
+    method:"post",
+    url:'/user/register',
+    data:"id="+id+"&pwd="+pwd,
+    dataType:"json",
+    success:function(json){
+
+    }
+  })
+}
+
+var pwdConfirm = function(){
+  var pwd = $("input[name='pwd']").val();
+  var cpwd = $("input[name='confirmpwd']").val();
+  if (pwd === cpwd) {
+    return true;
+  }else {
+    return false;
+  }
+}
